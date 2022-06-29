b2e325c3a150fb323dd191ef63ca9c4bd085cf3c
Priyanshi Sharma <priyanshisharma085@gmail.com>
2022-06-30 03:46:12 +0530

    first commit

---
+++ question3.js
@@ -0,0 +1,19 @@
+// Q3. Check the conditons
+// You are given two numbers 
+// A
+//  and 
+// B
+// . You need to do the following checks:
+
+// 1. if both are divisible by 10 console true.
+// 2. if both are not divisible by 10 console false.
+// 3. if one of them only is divisible by 10 console true.
+// Use operator to do this.
+
+// Note: You have to complete Check function. No need to take any input.
+
+// const Check = (A, B) => 
+// {
+//   return ((A % 10 === 0 || B % 10 === 0) || (A % 10 === 0 && B % 10)) ? true : false;
+// };
+// console.log(Check);
---
+++ question8.js
@@ -0,0 +1,22 @@
+// Q8. Marks Calculator
+// Shyam has got his marks in three subjects as 
+// A
+// , 
+// B
+// , and 
+// C
+//  (out of 100).Write a program to calculate his total marks and his average.
+
+// Note: You have to complete Sum and Average functions. No need to take any input.
+
+// const Sum = (A, B, C) => 
+// {
+//  return (A + B +C);
+// };
+// console.log(Sum);
+
+// const Average = (A, B, C) => 
+// {
+//  return ((A + B + C)/3);
+// };
+// console.log(Average);
